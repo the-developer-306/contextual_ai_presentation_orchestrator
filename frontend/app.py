41a22649c696e55c482fd6e2c04a5e12e8ba31b4
Pratham Khanna <whilealivecode127.0.0.1@gmail.com>
2025-09-13 16:19:45 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -4,7 +4,8 @@ import json
 import os
 
 # FastAPI backend URL
-API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000/api")
+API_BASE_URL = os.getenv("API_BASE_URL")
+# API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000/api")
 
 
 st.set_page_config(
